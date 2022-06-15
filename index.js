5b74a8e35cfbdcdc15a613a230502bfb24b31439
LuisRG98 <70450227+LuisRG98@users.noreply.github.com>
2022-06-15 15:24:45 -0400

    dice challenge

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber)
+
+document.getElementsByClassName("img1")[0].src = "/images/dice"+randomNumber+".png";
+document.getElementsByClassName("img2")[0].src = "/images/dice"+randomNumber2+".png";
+
+if (randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+}
+else if(randomNumber<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins"
+}
+else{
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+}
