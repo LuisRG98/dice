cc07931181249658761e1b7b135a27729d8dcff2
LuisRG98 <70450227+LuisRG98@users.noreply.github.com>
2022-06-15 15:28:37 -0400

    neww

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var randomNumber = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber)
 
-document.getElementsByClassName("img1")[0].src = "./images/dice"+randomNumber+".png";
-document.getElementsByClassName("img2")[0].src = "./images/dice"+randomNumber2+".png";
+document.getElementsByClassName("img1")[0].src = "../images/dice"+randomNumber+".png";
+document.getElementsByClassName("img2")[0].src = "../images/dice"+randomNumber2+".png";
 
 if (randomNumber>randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 wins"
